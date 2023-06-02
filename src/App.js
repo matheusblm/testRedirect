39f9d68abb67f10bd52c8d47fc7c26fdd76d73e0
matheusblm <matheus_bueno10@hotmail.com>
2023-06-02 14:02:32 -0300

    test 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import "./App.css";
 import { useEffect } from "react";
 import { isMobile, isIOS } from "react-device-detect";
+
 function App() {
   useEffect(() => {
+    const storeName = new URLSearchParams(window.location.search).get("store");
     if (isMobile) {
       // Redirect to the app store for mobile devices
-      window.location.href = isIOS ? `docgsx://` : `docgsx://`;
+      if (storeName) {
+        window.location.href = isIOS
+          ? `docgsx://petshop=${storeName}`
+          : `docgsx://petshop=${storeName}`;
+      } else {
+        // Redirect to the home page if store name is not provided
+        window.location.href = "docgsx://";
+      }
     }
   }, []);
 
   return (
     <div>
-      <p>Welcome to our desktop website!</p>
-      <a hre="docgsx://">Welcome to our desktop website!</a>
+      {!isMobile && (
+        <div>
+          <p>Welcome to our desktop website!</p>
+          <p>Please download our app:</p>
+          <a href="market://details?id=com.docgsx">Download on Android</a>
+          <a href="itms-apps://apps.apple.com/tr/app/docgx/id1530582505">
+            Download on iOS
+          </a>
+        </div>
+      )}
     </div>
   );
 }
