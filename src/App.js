a686fc3a57f24f94ad4897ab3fe8bdc74abcc697
matheusblm <matheus_bueno10@hotmail.com>
2023-06-02 14:09:40 -0300

    test 4

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { isMobile, isIOS } from "react-device-detect";
 
 function App() {
+  const storeName = new URLSearchParams(window.location.search).get("store");
   useEffect(() => {
-    const storeName = new URLSearchParams(window.location.search).get("store");
     if (isMobile) {
       // Redirect to the app store for mobile devices
       if (storeName) {
@@ -20,16 +20,15 @@ function App() {
 
   return (
     <div>
-      {!isMobile && (
-        <div>
-          <p>Welcome to our desktop website!</p>
-          <p>Please download our app:</p>
-          <a href="market://details?id=com.docgsx">Download on Android</a>
-          <a href="itms-apps://apps.apple.com/tr/app/docgx/id1530582505">
-            Download on iOS
-          </a>
-        </div>
-      )}
+      <div>
+        <p>Welcome to our desktop website!</p>
+        <p>Please download our app:</p>
+        <a href="market://details?id=com.docgsx">Download on Android</a>
+        <a href="itms-apps://apps.apple.com/tr/app/docgx/id1530582505">
+          Download on iOS
+        </a>
+      </div>
+      {storeName && storeName}
     </div>
   );
 }
